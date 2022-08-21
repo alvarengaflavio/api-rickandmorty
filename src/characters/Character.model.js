3e42bbaa4187fb33a490dd29bf837cc79425eb63
alvalenda <banysan@msn.com>
2022-08-21 07:49:04 -0300

    character model

---
+++ src/characters/Character.model.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+const { Schema, model } = mongoose;
+
+const CharacterSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+});
