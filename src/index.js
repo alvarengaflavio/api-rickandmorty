3a40fe3a83b75881613c3745a1e2dcf26a5fd6e1
alvalenda <banysan@msn.com>
2022-08-21 07:39:22 -0300

    express listening

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 
 const port = 3001;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
