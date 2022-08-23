dd9170719ab37937df8f7c18f0c43935c7d7e009
alvalenda <banysan@msn.com>
2022-08-23 18:00:37 -0300

    query: limit, offset

--- src/characters/character.middleware.js
+++ src/characters/character.middleware.js
@@ -30,7 +30,7 @@ export class CharacterMiddleware {
 
   static validateQuery(req, res, next) {
     try {
-      const defaultQuery = { name: '', offset: 0, limit: 8 };
+      const defaultQuery = { name: '', offset: 0, limit: 6 };
       let { offset, limit, name } = req.query;
       [offset, limit] = [offset, limit].map(Number);
       if (!name) name = defaultQuery.name;
